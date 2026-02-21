979fdd555f00edb61e919c6cc2989fc9c18d1c39
vranken <vranken@fev.io>
2026-02-21 11:33:22 +0100

    Fix: Missing files added to source control

---
+++ FBL/code/system/CCP/swr_softwareReset.c
@@ -0,0 +1,109 @@
+/**
+ * @file swr_softwareReset.c
+ * Trigger a software reset, a functional, non-destructive reset, which doesn't kill the
+ * RAM contents, so that we can exchange information between application and FBL across
+ * resets.
+ *
+ * Copyright (C) 2026 Peter Vranken (mailto:Peter_Vranken@Yahoo.de)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/* Module interface
+ * Local functions
+ */
+
+/*
+ * Include files
+ */
+
+#include "swr_softwareReset.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "MPC5748G.h"
+#include "rtos.h"
+
+/*
+ * Defines
+ */
+ 
+
+/*
+ * Local type definitions
+ */
+ 
+ 
+/*
+ * Local prototypes
+ */
+ 
+ 
+/*
+ * Data definitions
+ */
+ 
+ 
+/*
+ * Function implementation
+ */
+
+/**
+ * Start the countdown till a functional reset.\n
+ *   To end up with a reset, this function should be called regularly once a Millisecond
+ * until reset. A short delay is implemented such that a feedback can still be written to
+ * the console.
+ */
+void swr_osSoftwareReset(void)
+{
+    static uint16_t tiTillReset_ = 1000u;
+    
+    if(tiTillReset_ == 1000u)
+        iprintf("Restarting by SW reset in 1s\r\n");
+
+    /* Give console output the time to show this message. */
+    if(tiTillReset_ == 0u)
+    {
+        /* Restart the application by reset. The information, whether and what to start
+           is passed in SRAM to the FBL across the reset. The startup code of the FBL
+           will evaluate the SRAM location. */
+        rtos_osSuspendAllInterrupts();
+        
+        /* Write arguments of reset at hard-coded address into RAM. We must do this
+           only after suspending all IRQ handling, because we may corrupt the memories
+           of some other part of the SW. No other piece of SW must ever get the chance
+           to execute once we get here. */
+        extern const uint8_t const ld_memRamStart[0];
+        uint32_t *pResetInfo = (uint32_t*)ld_memRamStart;
+        * pResetInfo++ = 0xDeadBeef;
+        * pResetInfo   = 0xDeafBee;
+
+        /* Request mode transition to RESET mode (0x0) by write of key, followed by
+           inverted key. RM48 38.3.2, pp.1096f. */
+        MC_ME->MCTL = MC_ME_MCTL_TARGET_MODE(0x0)
+                      | MC_ME_MCTL_KEY(0x5AF0);
+        MC_ME->MCTL = MC_ME_MCTL_TARGET_MODE(0x0)
+                      | MC_ME_MCTL_KEY(0xA50F);
+
+        /* Halt all SW execution until the reset is executed. */
+        while(true)
+            ;
+    }
+    else
+        -- tiTillReset_;
+
+} /* swr_osSoftwareReset */
---
+++ FBL/code/system/CCP/swr_softwareReset.h
@@ -0,0 +1,58 @@
+#ifndef SWR_SOFTWARERESET_INCLUDED
+#define SWR_SOFTWARERESET_INCLUDED
+/**
+ * @file swr_softwareReset.h
+ * Definition of global interface of module swr_softwareReset.c
+ *
+ * Copyright (C) 2026 Peter Vranken (mailto:Peter_Vranken@Yahoo.de)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Include files
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+/*
+ * Defines
+ */
+
+
+/*
+ * Global type definitions
+ */
+
+
+/*
+ * Global data declarations
+ */
+
+
+/*
+ * Global prototypes
+ */
+
+/* Start the countdown till a functional reset. */
+void swr_osSoftwareReset(void);
+
+/*
+ * Global inline functions
+ */
+
+
+#endif  /* SWR_SOFTWARERESET_INCLUDED */
